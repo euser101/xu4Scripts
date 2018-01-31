bbcc48490c13e5f93057beaeaf5d42eee2b6c69f
euser101 <eric.studer@lernende.bfo-vs.ch>
2018-01-31 15:02:22 +0100

    Inital Script

---
+++ benchmark-logger.sh
@@ -0,0 +1,34 @@
+#!/bin/sh
+
+THREADS=$(nproc)
+SEPARATE="\n%s\n%s\n"
+
+checkDependencies() {
+    if hash sysbench 2>/dev/null; then
+	printf $SEPARATE ""
+    else
+        printf $SEPARATE "Installing Dependencies..."
+        sudo apt-get -y install sysbench
+    fi
+	runBenchmark
+}
+
+runBenchmark() {
+	#Testing CPU, RAM and Disk
+	printf $SEPARATE "Starting Benchmarks..."
+	TIME=$(date -d "today" +"%Y%m%d%H%M")
+	FILENAME=results-$TIME.log
+	sysbench --threads=$THREADS --test=cpu --cpu-max-prime=1000 run >> $FILENAME 2>&1
+	sysbench --test=mutex --mutex-num=1 --mutex-locks=50000000 --mutex-loops=1 run >> $FILENAME 2>&1
+	sysbench --test=memory --memory-block-size=1M --memory-total-size=5G run >> $FILENAME 2>&1
+	dd bs=1M count=256 if=/dev/zero of=test conv=fdatasync >> $FILENAME 2>&1
+
+	printResults
+}
+
+printResults() {
+	printf $SEPARATE "Logged Results into File $FILENAME"
+	cat $FILENAME
+}
+
+checkDependencies
