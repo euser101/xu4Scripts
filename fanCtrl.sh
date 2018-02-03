78c42ede747b2009c6a7edf5c302ef325e547bdd
euser101 <eric.studer@lernende.bfo-vs.ch>
2018-02-03 21:20:37 +0100

    Initial fancontrol script

---
+++ fanCtrl.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+#PWM-Fan speeds at trip temps
+PWM_VALS="60 80 90 100" # Values have to increase
+
+PWM_CONFIG="/sys/devices/platform/pwm-fan/hwmon/hwmon0/fan_speed"
+CUR_SPEED="/sys/devices/platform/pwm-fan/hwmon/hwmon0/pwm1"
+
+sudo chmod a+rw $PWM_CONFIG $CUR_SPEED
+sudo echo "$PWM_VALS" > $PWM_CONFIG
