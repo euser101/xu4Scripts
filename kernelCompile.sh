d70c8624fc1f71a4a25fca31ed156da001e0dbfe
euser101 <eric.studer@lernende.bfo-vs.ch>
2018-02-07 21:30:03 +0100

    Check for module install

--- kernelCompile.sh
+++ kernelCompile.sh
@@ -10,9 +10,7 @@ checkVersion() {
 	   apt install -y build-essential git
 	   apt-mark hold bootini linux-image*
 	   git clone --depth 1 --branch odroidxu4-4.14.y https://github.com/euser101/linux/tree/odroidxu4-4.14.y
-	   cd linux
 	else
-	   cd linux
 	   git pull
 	   make clean
 	fi
@@ -21,21 +19,34 @@ checkVersion() {
 }
 
 compile() {
+	cd ~/linux
 	make odroidxu4_defconfig
 	make -j8
 
-	updateImages
+	installModules
+}
+
+installModules() {
+	if [ make modules_install -eq 0 ]; then
+	  updateImages
+	else
+	  echo "Could not compile" >&2
+	fi	
 }
 
 updateImages() {
 	cp arch/arm/boot/zImage arch/arm/boot/dts/exynos5422-odroidxu4.dtb /media/boot/
-	make modules_install
 	NEW=$(make kernelrelease)
 	cp arch/arm/boot/zImage /boot/zImage-$NEW
+
+	updateInitrd
+}
+
+updateInitrd() {
 	update-initramfs -c -k $NEW
 	mkimage -A arm -O linux -T ramdisk -C none -a 0 -e 0 -n uInitrd -d /boot/initrd.img-$NEW /boot/uInitrd-$NEW
 	cp /boot/uInitrd-$NEW /media/boot/uInitrd
-
+	
 	cleanup
 }
 
